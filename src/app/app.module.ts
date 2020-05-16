c12e5c6217ce8801e7b184a9c3c05a09a416a6b4
sohambose <sohamboseofficial@gmail.com>
2020-05-16 17:28:23 +0530

    Removed unused declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './home/dashboard/dashboard.component';
 
 import { HttpClientModule } from '@angular/common/http';
-import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MemberModule } from './member/member.module';
@@ -16,13 +13,14 @@ import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+
     MemberModule,
     AuthModule,
     HomeModule
